cce3754464ee9d3389dea44686bda79737cd0c43
CarlosCode502 <carloshl.code21@gmail.com>
2023-09-05 09:18:35 -0600

    Se corrigieron algunos errores, ahora solo falta corregir el footer y la página de inicio.

--- MenuSlider_HTML_CSS_JS/wwwroot/js/site.js
+++ MenuSlider_HTML_CSS_JS/wwwroot/js/site.js
@@ -4,9 +4,9 @@
 // Write your JavaScript code.
 //Obtener y asignar el menu para poder obtener los datos
 const menu = document.getElementById('nav_menu_id');//Se debe especificar el id del menú
-const indicador = document.getElementById('indicador_id');//especificamos el id del identificador
+let indicador = document.getElementById('indicador_id');//especificamos el id del identificador
 //va a contener todas las secciones que tiene mi pagina con "querySelectorAll"
-const secciones = document.querySelectorAll('.seccion, #Inicio, #Contacto');//ADJUNTAR TODAS NUESTRAS SECCIONES SIN IMPORTAR EL TIPO TENER CUIDADO
+let secciones = document.querySelectorAll('#Inicio, #Perfil, #Habilidades, #Proyectos_recientes, #Contacto');//ADJUNTAR TODAS NUESTRAS SECCIONES SIN IMPORTAR EL TIPO TENER CUIDADO
 /*console.log(secciones);*///obtenemos un nodo que contiene los elementos de secciones
 //Obtener el tamaño de nuestro indicador (let) ya que el tamaño va a cambiar
 //accedemos al indicador actual (no a todos)
@@ -18,6 +18,9 @@ indicador.style.width = tamanioIndicador + 'px';
 //Para obtener el index de la sección actual o activa
 let indexSeccionAcctiva;
 
+//Personal calcular el threshold y rootmargin
+let rootmargin_V = '-100px 0px 0px 0px';
+let threshold_V = 0.65;
 
 //Identificar en que seccion se encuentra
 //Para esto se va a utilizar (IntersectorObserver) para detectar en que seccion estoy
@@ -38,7 +41,7 @@ const observer = new IntersectionObserver((entradas, observer) => {
             /*console.log(indexSeccionAcctiva);*/
 
             let tamanioIndicador = menu.querySelector('a').offsetWidth;
-
+            //console.log('Este es el tam desde el observer' + tamanioIndicador);
             //if (indexSeccionAcctiva == 0) {
             //    indicador.style.transform = `translateX(-500px)`;
             //}
@@ -47,19 +50,26 @@ const observer = new IntersectionObserver((entradas, observer) => {
             /*se ejecuta por cada entrada se muestra en consola*/
             /*console.log(entrada.target)*///Imprime todas las entradas según se este recorriendo
 
+            //indicador > estilos > ancho = tm del indicador
+            indicador.style.width = tamanioIndicador + 'px';
+
             //accedemos al estilo del indicador luego a su transform para transformarlo en traslare en eje de x
             /*indicador.style.transform = 'translateX(500px)';*///CAMBIAMOS PARA ADMITIR VALORES DE JS
             indicador.style.transform = `translateX(${tamanioIndicador * indexSeccionAcctiva}px)`;//Alt+96 para añadir(Backsticks)
+            /*console.log('la sección activa su indice es: ' + indexSeccionAcctiva);*/
         }
     });
 }, {
     //Un segundo parametros en donde es posible indicar los márgenes
     //Esto para que tome en centa el espacio del nav ya que este se encuentra sobre 80 px
-    rootMargin: '80px 0px 0px 0px',
+    rootMargin: rootmargin_V,
     //Permite ejecutar el código según la parte de la sección que se muestre
     //Es decir que permite definir si cambiar el indicador cuando se este en el 20% de la sección y sucesivamente
-    threshold: 0.90//Equivale al 20%
+    threshold: threshold_V//Equivale al 60%
+    /*60% para pantallas grandes*/
 });
+/*console.log(threshold_V, rootmargin_V);*/
+
 
 //OBSERVADOR PARA EL INICIO (min 48:28)
 /*observer.observe(document.getElementById('.hero'));*/
@@ -76,17 +86,21 @@ secciones.forEach(seccion => observer.observe(seccion));
 
 
 //Evento para detectar cuando la pantalla cambie de tamaño
-const onResize = () => {
+let onResize = () => {
+    /*console.log('pantalla cambio a' + onResize);*/
     //Calcula el nuevo tamaño que tendra nuestro indicador
     tamanioIndicador = menu.querySelector('a').offsetWidth;
+
     //console.log('el tamaño del indicador es:' + tamanioIndicador);
 
-    //cambiamos el tamaño del indicador 
+    //cambiamos el tamaño del indicador
     indicador.style.width = `${tamanioIndicador}px`;
+/*    console.log('El valor del nuevo resize es' + indicador.style.width)*/
     //console.log(indicador.style.width = `${tamanioIndicador}px`);
     //Se vuelve a posicionar el indicador
     indicador.style.transform = `translateX(${tamanioIndicador * indexSeccionAcctiva}px)`;
     /*console.log(indicador.style.transform = `translateX(${tamanioIndicador * indexSeccionAcctiva}px)`);*/
+    /*threshold_V = 0.*/
 }
 
 
